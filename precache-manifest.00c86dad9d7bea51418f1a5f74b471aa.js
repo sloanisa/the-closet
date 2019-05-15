c40a19bcfc8b26259b64f6e9d41eaea03354abdf
Anisa Sloan <sloanisa@gmail.com>
2019-05-14 20:22:17 -0400

    Updates

---
+++ precache-manifest.00c86dad9d7bea51418f1a5f74b471aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aefd01595da4f920d39d62b97a4b1aa",
+    "url": "/the-closet/index.html"
+  },
+  {
+    "revision": "0eedda644a1265b5c45a",
+    "url": "/the-closet/static/css/main.20c4b60e.chunk.css"
+  },
+  {
+    "revision": "df6b954a2cc68b4f00c3",
+    "url": "/the-closet/static/js/2.30661e94.chunk.js"
+  },
+  {
+    "revision": "0eedda644a1265b5c45a",
+    "url": "/the-closet/static/js/main.2dcc45cd.chunk.js"
+  },
+  {
+    "revision": "c65d37539334cbdf4218",
+    "url": "/the-closet/static/js/runtime~main.9f1b5408.js"
+  }
+]);
