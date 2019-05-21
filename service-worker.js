b5923247f0298ce8eee889347391a7567a9d9989
Anisa Sloan <sloanisa@gmail.com>
2019-05-21 19:20:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/the-closet/precache-manifest.67b9605661150a2fefeabc628da74b0b.js"
+  "/the-closet/precache-manifest.f1d8eeb907a75227d3d7776398bb999f.js"
 );
 
 self.addEventListener('message', (event) => {
