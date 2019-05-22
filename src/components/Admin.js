2bf47fc3b9acb0ae4df65db9c0d2058d8054ddfb
Phil Sinatra <code@philsinatra.com>
2019-05-22 12:58:50 -0400

    Fix Store Items Data Path

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Inventory from './Inventory';
 
-const dataUrl = '../store-items.json';
+const dataUrl = './store-items.json';
 
 class Admin extends Component {
-
   render() {
     return (
       <div className="admin-container">
-      <Inventory data-url={dataUrl} />
-    </div>
+        <Inventory data-url={dataUrl} />
+      </div>
     );
   }
 }
